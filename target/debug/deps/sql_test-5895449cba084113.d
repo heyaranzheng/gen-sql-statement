b4c60d389e56904dce7e014629b177133ab470ef
Aran Z <heyaranzheng@hotmail.com>
2026-05-23 15:55:13 +0800

    update

---
+++ target/debug/deps/sql_test-5895449cba084113.d
@@ -0,0 +1,5 @@
+/home/aran/gen-sql-statement/target/debug/deps/sql_test-5895449cba084113.d: tests/sql_test.rs
+
+/home/aran/gen-sql-statement/target/debug/deps/libsql_test-5895449cba084113.rmeta: tests/sql_test.rs
+
+tests/sql_test.rs:
---
+++ target/debug/deps/vcpkg-394aa79480fa29ca.d
@@ -0,0 +1,7 @@
+/home/aran/gen-sql-statement/target/debug/deps/vcpkg-394aa79480fa29ca.d: /home/aran/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/vcpkg-0.2.15/src/lib.rs
+
+/home/aran/gen-sql-statement/target/debug/deps/libvcpkg-394aa79480fa29ca.rlib: /home/aran/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/vcpkg-0.2.15/src/lib.rs
+
+/home/aran/gen-sql-statement/target/debug/deps/libvcpkg-394aa79480fa29ca.rmeta: /home/aran/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/vcpkg-0.2.15/src/lib.rs
+
+/home/aran/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/vcpkg-0.2.15/src/lib.rs:
